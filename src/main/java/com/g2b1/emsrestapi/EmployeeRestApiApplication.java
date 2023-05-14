e42ab12acc8da528d377711d3cadd3630879ba9d
Vivek Chandra <chandravivek52@gmail.com>
2023-05-14 09:11:57 +0530

    inital-commit

---
+++ src/main/java/com/g2b1/emsrestapi/EmployeeRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.g2b1.emsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Spring-Boot Application starts here!
+@SpringBootApplication
+public class EmployeeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestApiApplication.class, args);
+	}
+
+}
